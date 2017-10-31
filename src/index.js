775668d38d48f4f7f1fb9d390ca9e2cdcb8414cd
kidman90 <kacprzak90@gmail.com>
2017-10-31 21:50:16 +0100

    06 Create a Redux Store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store';
 
-
-const state = {
-  todos: [
-    { id: 1, name: 'Render static UI', isComplete: true },
-    { id: 2, name: 'Create initial state', isComplete: true },
-    { id: 3, name: 'Render based on state', isComplete: false }
-  ]
-};
-ReactDOM.render(<App todos={state.todos} />, document.getElementById('root'));
+const state = store.getState();
+ReactDOM.render(<App {...state} />, document.getElementById('root'));
 registerServiceWorker();
