3d64c3df225675e5e2bb0f93c14341f1528ae84c
kidman90 <kacprzak90@gmail.com>
2017-11-01 17:07:57 +0100

    09 Make Dispatching Redux Actions More Reliable With Action Creators

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -7,9 +7,14 @@ const initState = {
   currentTodo: ''
 };
 
+const TODO_ADD = 'TODO_ADD';
+const CURRENT_UPDATE = 'CURRENT_UPDATE';
+
+export const updateCurrent = (val) => ({ type: CURRENT_UPDATE, payload: val });
+
 export default (state = initState, action) => {
   switch (action.type) {
-    case 'TODO_ADD':
+    case TODO_ADD:
       return {
         ...state,
         todos: [
@@ -17,7 +22,7 @@ export default (state = initState, action) => {
           action.payload
         ]
       };
-    case 'CURRENT_UPDATE':
+    case CURRENT_UPDATE:
       return {
         ...state,
         currentTodo: action.payload
