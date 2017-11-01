23f573bc6ad443c229dce053ba8c7a139c285c4b
kidman90 <kacprzak90@gmail.com>
2017-11-01 17:12:24 +0100

    10 Create Redux Dispatch Functions From Action Creators with bindActionCreators

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store';
+import { bindActionCreators } from 'redux';
 import { updateCurrent } from './reducers/todo';
 
-const todoChangeHandler = (val) => store.dispatch(updateCurrent(val));
+const actions = bindActionCreators({ updateCurrent }, store.dispatch);
 
 const render = () => {
   const state = store.getState();
   ReactDOM.render(<App
     todos={state.todos}
     currentTodo={state.currentTodo}
-    changeCurrent={todoChangeHandler}
+    changeCurrent={actions.updateCurrent}
   />, document.getElementById('root'));
 };
 render();
